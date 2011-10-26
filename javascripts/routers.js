ce0158fa9c94c0feb77910910a7d14d10e9520da
Geoffrey Hing <geoff@terrorware.com>
2011-10-26 11:29:18 -0400

    Added show listing to venue.

--- javascripts/routers.js
+++ javascripts/routers.js
@@ -21,18 +21,34 @@ App.Routers.Sets = Backbone.Router.extend({
 
     venue: function(slug) {
         // Fix case where venue was named differently in sets vs. venues
+        var venueSlug = slug;
     	if (slug == "bar-1982") {
-	    slug = "1982";
+	    venueSlug = "1982";
 	}
         else if (slug == "durty-nelly-s") {
-	    slug = "durty-nelly-s-pub";
+	    venueSlug = "durty-nelly-s-pub";
  	}
     	var venues = new App.Collections.Venues();
 	venues.fetch({
             success: function() {
-                new App.Views.Venue({ venue: venues.find(function(venue) { 
-		    return venue.get('slug') == slug;
-		})});
+                var sets = new App.Collections.Sets();
+                sets.fetch({
+                    success: function() {
+                        var venue = venues.find(function(venue) { 
+                            return venue.get('slug') == venueSlug;
+                        });
+                        console.debug(sets);
+                        var venueSets = sets.filter(function(set) {
+                            return set.get('venue_slug') == slug;
+                        });
+                        new App.Views.Venue({ venue: venue, sets: venueSets });
+                    },
+                    error: function() {
+                        // QUESTION: What does Backbone do with the error class?
+                        new Error({ message: "Error loading venue sets." });
+                        console.error('Error loading venue sets.');
+                    }
+                });
             },
             error: function() {
 		// QUESTION: What does Backbone do with the error class?
--- javascripts/views.js
+++ javascripts/views.js
@@ -1,3 +1,48 @@
+App.Views.setTemplate = "<div class='{{ container_class }}'>\n" +
+        "    <a name='{{ slug }}'></a>\n" +
+        "    <div class='band'>{{ band }}</div>\n" +
+    	"    <div class='start-time'>{{ start_time }}</div>\n" +
+        "    <div class='time-sep'>&nbsp;-&nbsp;</div>" +
+        "    <div class='end-time'>{{ end_time }}</div>\n" +
+        "    <div class='venue'><a href='#venues/{{ venue_slug }}'>{{ venue }}</a></div>\n" +
+        "</div>\n";
+
+App.Views.renderSets = function(sets) {
+    var out = "";
+    var template = _.template(App.Views.setTemplate);
+    var dateStr = "";
+    var currentDateTime = new Date();
+    var zebraClass = "odd";
+    _.each(sets, function(set) {
+        startDate = set.get('start_date');
+        if (startDate != dateStr) {
+            dateStr = startDate;
+            out += "<h2>" + dateStr + "</h2>\n";
+        }
+        var containerClass = "set";
+        if (set.get('end_datetime').getTime() > currentDateTime.getTime()) {
+            containerClass += " ongoing-or-upcoming";	
+        }
+        containerClass += " " + zebraClass;
+        out += template({
+            container_class: containerClass, 
+            slug: set.get('slug'),
+            start_time: set.get('start_time'),
+            end_time: set.get('end_time'),
+            band: set.get('band'),
+            venue: set.get('venue'),
+            venue_slug: set.get('venue_slug')
+        }); 
+        if (zebraClass == "odd") {
+            zebraClass = "even";
+        }
+        else {
+            zebraClass = "odd";
+        }
+    });   
+    return out;
+};
+
 App.Views.Index = Backbone.View.extend({
     initialize: function() {
     	this.sets = this.options.sets;
@@ -10,41 +55,9 @@ App.Views.Index = Backbone.View.extend({
         _.templateSettings = {
 	    interpolate : /\{\{(.+?)\}\}/g
 	};	
-        var out = "";
-        var template = _.template(this.set_template);
-        var dateStr = "";
-	var currentDateTime = new Date();
-        var zebraClass = "odd";
 
-        this.sets.each(function(set) {
-            startDate = set.get('start_date');
-            if (startDate != dateStr) {
-                dateStr = startDate;
-                out += "<h2>" + dateStr + "</h2>\n";
-            }
-	    var containerClass = "set";
-	    if (set.get('end_datetime').getTime() > currentDateTime.getTime()) {
-	        containerClass += " ongoing-or-upcoming";	
-	    }
-            containerClass += " " + zebraClass;
-            out += template({
-	    	container_class: containerClass, 
-                slug: set.get('slug'),
-                start_time: set.get('start_time'),
-                end_time: set.get('end_time'),
-                band: set.get('band'),
-                venue: set.get('venue'),
-		venue_slug: set.get('venue_slug')
-            }); 
-            if (zebraClass == "odd") {
-                zebraClass = "even";
-            }
-            else {
-                zebraClass = "odd";
-            }
-	});   
 
-        $(this.el).html(out);
+        $(this.el).html(App.Views.renderSets(this.sets.models));
         $('#app').html(this.el); 
 	var newPosition = $('.ongoing-or-upcoming').offset();
         window.scrollTo(0, newPosition.top);	
@@ -52,17 +65,46 @@ App.Views.Index = Backbone.View.extend({
 	return this;
     },
 
-    set_template: "<div class='{{ container_class }}'>\n" +
-        "    <a name='{{ slug }}'></a>\n" +
-        "    <div class='band'>{{ band }}</div>\n" +
-    	"    <div class='start-time'>{{ start_time }}</div>\n" +
-        "    <div class='time-sep'>&nbsp;-&nbsp;</div>" +
-        "    <div class='end-time'>{{ end_time }}</div>\n" +
-        "    <div class='venue'><a href='#venues/{{ venue_slug }}'>{{ venue }}</a></div>\n" +
-        "</div>\n"
 });
 
 App.Views.Venue = Backbone.View.extend({
+    initialize: function() {
+    	this.venue = this.options.venue;
+    	this.sets = this.options.sets;
+	this.render();
+    },
+    
+
+    render: function() {
+        // Make underscore do Moustache style templates
+        _.templateSettings = {
+	    interpolate : /\{\{(.+?)\}\}/g
+	};	
+        var out = "";
+        var template = _.template(this.venue_template);
+	var address = this.venue.get('street') + ', ' + this.venue.get('city') + ', ' + this.venue.get('state');
+        out += template({
+	    name: this.venue.get('name'),
+	    street: this.venue.get('street'),
+            address: address
+        }); 
+        out += App.Views.renderSets(this.sets);
+        $(this.el).html(out);
+        $('#app').html(this.el); 
+
+	return this;
+    },
+
+    venue_template: "<div class='venue'>\n" +
+        "    <h2 class='name'>{{ name }}</h2>\n" +
+	"    <p class='street'>{{ street }}</p>\n" + 
+        '    <p class="directions"><a href="http://maps.google.com/maps?saddr=&daddr={{ address }}">Directions</a>\n' + 
+        "</div>\n" +
+        "<div id='map-canvas' style='width: 100%; height: 100%;'></div>\n"
+
+});
+
+App.Views.VenueMap = Backbone.View.extend({
     initialize: function() {
     	this.venue = this.options.venue;
 	this.render();
@@ -75,7 +117,7 @@ App.Views.Venue = Backbone.View.extend({
 	    interpolate : /\{\{(.+?)\}\}/g
 	};	
         var out = "";
-        var template = _.template(this.venue_template);
+        var template = _.template(this.venue_map_template);
         var infowindowTemplate = _.template(this.venue_infowindow_template); 
         out += template({
 	    name: this.venue.get('name'),
@@ -121,14 +163,7 @@ App.Views.Venue = Backbone.View.extend({
 	return this;
     },
 
-    /*
-    venue_template: "<div class='venue'>\n" +
-        "    <h2 class='name'>{{ name }}</h2>\n" +
-	"    <span class='street'>{{ street }}</span>\n" + 
-        "</div>\n" +
-        "<div id='map-canvas' style='width: 100%; height: 100%;'></div>\n"
-    */
-    venue_template: "<div id='map-canvas' style='width: 100%; height: 100%;'></div>\n",
+    venue_map_template: "<div id='map-canvas' style='width: 100%; height: 100%;'></div>\n",
  
     venue_infowindow_template : '<div class="info-window">\n' + 
         '    <h1 class="venue-name">{{ name }}</h1>\n' +
